f4e571d7042eb6f3bda9d11847970448b71ae1bc
Silvia Martinez <silvia.martinez@Silvias-MacBook-Pro-2.local>
2023-08-04 12:03:27 +0200

    Add menu component in App and add logic to use it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Menu from "./components/Menu";
+
+const dataMenu = [
+  { title: "title 1", text: "text 1 id: 1", id: 1 },
+  { title: "title 2", text: "text 2 id: 2", id: 2 },
+  { title: "title 3", text: "text 3 id: 7", id: 7 },
+  { title: "title 4", text: "text 4 id: 99", id: 99 },
+  { title: "title 5", text: "text 5 id: 4", id: 4 },
+];
+
+const initialState = dataMenu.map((menu, index) => ({
+  ...menu,
+  expanse: index === 0,
+}));
 
 function App() {
+  const [dataComponent, setDatacomponent] = useState(initialState);
+
+  const setDataMenuComponent = (id) => {
+    setDatacomponent((prevMenu) =>
+      prevMenu.map((menu) =>
+        menu.id === id
+          ? { ...menu, expanse: !menu.expanse }
+          : { ...menu, expanse: false }
+      )
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {dataComponent
+        .sort((a, b) => a.id - b.id)
+        .map((d, index) => {
+          return (
+            <div key={index}>
+              <Menu
+                data={d}
+                onClick={() => setDataMenuComponent(d.id)}
+                expanse={d.expanse}
+              />
+            </div>
+          );
+        })}
     </div>
   );
 }
